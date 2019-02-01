6a288e154140c9eaba275ad225bfd89cee9ffd63
zacharybys <zachary.bys@gmail.com>
2019-01-31 19:45:57 -0500

    [#7] add response and new location for secret

--- app/src/main/java/org/wikipedia/search/ImageSearch.java
+++ app/src/main/java/org/wikipedia/search/ImageSearch.java
@@ -1,28 +1,66 @@
 package org.wikipedia.search;
 
+import android.content.Context;
+import android.util.Log;
+
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.services.vision.v1.Vision;
 import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.AnnotateImageResponse;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import org.wikipedia.R;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 
 public class ImageSearch {
     private Vision vision;
 
-    public ImageSearch() {
+    //Pass in the application context with this.getApplicationContext() to access resources
+    public ImageSearch(Context applicationContext) {
         Vision.Builder visionBuilder = new Vision.Builder(
                 new NetHttpTransport(),
                 new AndroidJsonFactory(),
                 null);
 
         visionBuilder.setVisionRequestInitializer(
-                new VisionRequestInitializer("YOUR_API_KEY"));
+                new VisionRequestInitializer(applicationContext.getString(R.string.google_vision_api_key)));
 
         vision = visionBuilder.build();
     }
 
-    public String searchPhoto(byte[] photo) {
+    public String searchPhoto(byte[] photo) throws IOException {
         String result = "";
 
+        Image inputImage = new Image();
+        inputImage.encodeContent(photo);
+
+        Feature desiredFeature = new Feature();
+        desiredFeature.setType("WEB_DETECTION");
+
+        AnnotateImageRequest request = new AnnotateImageRequest();
+        request.setImage(inputImage);
+        request.setFeatures(Arrays.asList(desiredFeature));
+
+        BatchAnnotateImagesRequest batchRequest =
+                new BatchAnnotateImagesRequest();
+
+        batchRequest.setRequests(Arrays.asList(request));
+
+        BatchAnnotateImagesResponse batchResponse =
+                vision.images().annotate(batchRequest).execute();
+
+        AnnotateImageResponse response = batchResponse.getResponses()
+                .get(0);
+
+        Log.d("TEST", response.getTextAnnotations().toString());
 
         return result;
     }
