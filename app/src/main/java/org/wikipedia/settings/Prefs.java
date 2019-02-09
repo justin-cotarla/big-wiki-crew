e8a0eb07bd86d7ba02788fe2b84450b2847eba0b
Justin Cotarla <justin.cotarla@gmail.com>
2019-02-09 12:58:47 -0500

    fix style issues

--- app/src/main/java/org/wikipedia/settings/Prefs.java
+++ app/src/main/java/org/wikipedia/settings/Prefs.java
@@ -4,6 +4,7 @@ import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+
 import com.google.gson.reflect.TypeToken;
 
 import org.wikipedia.R;
