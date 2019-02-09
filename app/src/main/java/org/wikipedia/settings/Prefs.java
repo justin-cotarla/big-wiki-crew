dcd82a45dad3f3839cb1f9935ef1679f7d8fcab0
Justin Cotarla <justin.cotarla@gmail.com>
2019-02-08 19:01:01 -0500

    Decouple TextToSpeechService from dependencies

--- app/src/main/java/org/wikipedia/settings/Prefs.java
+++ app/src/main/java/org/wikipedia/settings/Prefs.java
@@ -4,8 +4,6 @@ import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
-import android.util.Log;
-
 import com.google.gson.reflect.TypeToken;
 
 import org.wikipedia.R;
