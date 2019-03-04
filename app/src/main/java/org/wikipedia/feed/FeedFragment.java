3c6012864593be31c6c6fb8758f0c3915e5c31f5
JamBerge <james.bergeron@hotmail.com>
2019-03-04 18:50:17 -0500

    [#27] implemented categories to the main feed page

--- app/src/main/java/org/wikipedia/feed/FeedFragment.java
+++ app/src/main/java/org/wikipedia/feed/FeedFragment.java
@@ -28,6 +28,7 @@ import org.wikipedia.WikipediaApp;
 import org.wikipedia.activity.FragmentUtil;
 import org.wikipedia.analytics.FeedFunnel;
 import org.wikipedia.feed.categories.CategoriesActivity;
+import org.wikipedia.feed.categories.CategoriesCardView;
 import org.wikipedia.feed.configure.ConfigureActivity;
 import org.wikipedia.feed.configure.ConfigureItemLanguageDialogView;
 import org.wikipedia.feed.configure.LanguageItemAdapter;
@@ -125,16 +126,6 @@ public class FeedFragment extends Fragment implements BackPressedHandler {
         feedView.setAdapter(feedAdapter);
         feedView.setCallback(feedCallback);
         feedView.addOnScrollListener(feedScrollListener);
-
-        // FIXME: TEMPORARY WAY OF VIEWING CATEGORIES
-        Button button = view.findViewById(R.id.temporary_categories_button);
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(requireActivity(), CategoriesActivity.class));
-            }
-        });
 
         swipeRefreshLayout.setColorSchemeResources(ResourceUtil.getThemedAttributeId(requireContext(), R.attr.colorAccent));
         swipeRefreshLayout.setOnRefreshListener(this::refresh);
@@ -521,6 +512,11 @@ public class FeedFragment extends Fragment implements BackPressedHandler {
         @Override
         public void onMoreContentSelected(@NonNull Card card) {
             startActivity(MostReadArticlesActivity.newIntent(requireContext(), (MostReadListCard) card));
+        }
+
+        @Override
+        public void onCategoriesClick(CategoriesCardView view) {
+        startActivity(CategoriesActivity.newIntent(requireActivity(), app.getWikiSite()));
         }
     }
 
