d76edcfb78a8ebb0f9389c4f1fe195fd9ec9dda8
Chris Woo <chriskfwoo@gmail.com>
2019-03-04 03:02:46 -0500

    [#44] Setup category result activity, category result listview

---
+++ app/src/main/java/org/wikipedia/feed/categories/CategoriesResultActivity.java
@@ -0,0 +1,34 @@
+package org.wikipedia.feed.categories;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import org.wikipedia.activity.SingleFragmentActivity;
+import org.wikipedia.search.SearchResult;
+
+import java.util.ArrayList;
+
+public class CategoriesResultActivity extends SingleFragmentActivity<CategoriesResultFragment> {
+    public static final String CATEGORY_TOPIC = "categoryTopic";
+    public static final String CATEGORY_RESULT = "categoryResult";
+
+    public static Intent newIntent(@NonNull Context context, @NonNull String categoryTopic, @NonNull ArrayList<SearchResult> categoryResult) {
+        return new Intent(context, CategoriesResultActivity.class)
+                .putExtra(CATEGORY_TOPIC, categoryTopic)
+                .putParcelableArrayListExtra(CATEGORY_RESULT, categoryResult);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setElevation(0f);
+    }
+
+    @Override
+    protected CategoriesResultFragment createFragment() {
+        return CategoriesResultFragment.newInstance(getIntent().getStringExtra(CATEGORY_TOPIC));
+    }
+}
---
+++ app/src/main/java/org/wikipedia/feed/categories/CategoriesResultFragment.java
@@ -0,0 +1,114 @@
+package org.wikipedia.feed.categories;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+
+import org.wikipedia.R;
+import org.wikipedia.history.HistoryEntry;
+import org.wikipedia.page.PageActivity;
+import org.wikipedia.page.PageTitle;
+import org.wikipedia.search.SearchResult;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
+import butterknife.Unbinder;
+
+import static org.wikipedia.feed.categories.CategoriesResultActivity.CATEGORY_RESULT;
+import static org.wikipedia.feed.categories.CategoriesResultActivity.CATEGORY_TOPIC;
+
+public class CategoriesResultFragment extends Fragment {
+    private Unbinder unbinder;
+    private String categoryTopic;
+    private ArrayList<SearchResult> categoryResult;
+
+    @BindView(R.id.categories_results_list) ListView categoryResultList;
+
+    @NonNull
+    public static CategoriesResultFragment newInstance(String categoryTopic) {
+        CategoriesResultFragment instance = new CategoriesResultFragment();
+        Bundle args = new Bundle();
+        args.putString(CATEGORY_TOPIC, categoryTopic);
+        instance.setArguments(args);
+        return instance;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_categories_result, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        categoryTopic = requireActivity().getIntent().getStringExtra(CATEGORY_TOPIC);
+        categoryResult = requireActivity().getIntent().getParcelableArrayListExtra(CATEGORY_RESULT);
+
+        categoryResultList = view.findViewById(R.id.categories_results_list);
+        CategoriesResultAdapter customerAdapter = new CategoriesResultAdapter();
+        categoryResultList.setAdapter(customerAdapter);
+
+        TextView result = view.findViewById(R.id.categories_results_topic);
+        result.setText(categoryTopic);
+        return view;
+    }
+
+    class CategoriesResultAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return categoryResult.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return categoryResult.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.item_categories_result, null);
+            TextView categoryTitle = (TextView) convertView.findViewById(R.id.categories_results_name);
+            categoryTitle.setText(categoryResult.get(position).getPageTitle().getText());
+
+            // TODO: Fix UI
+//            ViewUtil.loadImageUrlInto(convertView.findViewById(R.id.page_list_item_image),
+//                    result.getPageTitle().getThumbUrl());
+//
+//            System.out.println(result.getPageTitle().getThumbUrl());
+//            System.out.println(result.getPageTitle().getDescription());
+
+            return convertView;
+        }
+    }
+
+    private BaseAdapter getAdapter() {
+        return (BaseAdapter) categoryResultList.getAdapter();
+    }
+
+    @OnItemClick(R.id.categories_results_list) void onItemClick(ListView view, int position) {
+        PageTitle item = ((SearchResult) getAdapter().getItem(position)).getPageTitle();
+        HistoryEntry historyEntry = new HistoryEntry(item, HistoryEntry.SOURCE_CATEGORY);
+        startActivity(PageActivity.newIntentForCurrentTab(requireContext(), historyEntry, historyEntry.getTitle()));
+    }
+
+    @Override
+    public void onDestroyView() {
+        unbinder.unbind();
+        unbinder = null;
+        super.onDestroyView();
+    }
+}
