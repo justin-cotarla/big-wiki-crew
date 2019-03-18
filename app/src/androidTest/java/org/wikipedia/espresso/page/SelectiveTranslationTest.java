a1dd1024d8f5a22c29f5e9cc65e18d81e0b26341
Justin Cotarla <justin.cotarla@gmail.com>
2019-03-18 15:26:58 -0400

    [#15] implement UI test for selective translation

---
+++ app/src/androidTest/java/org/wikipedia/espresso/page/SelectiveTranslationTest.java
@@ -0,0 +1,166 @@
+package org.wikipedia.espresso.page;
+
+
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wikipedia.R;
+import org.wikipedia.main.MainActivity;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SelectiveTranslationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void selectiveTranslationTest() {
+        ViewInteraction linearLayout = onView(
+                allOf(withId(R.id.search_container),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fragment_feed_feed),
+                                        0),
+                                0),
+                        isDisplayed()));
+        linearLayout.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withId(R.id.search_src_text),
+                        childAtPosition(
+                                allOf(withId(R.id.search_plate),
+                                        childAtPosition(
+                                                withId(R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete.perform(replaceText("wikipedia"), closeSoftKeyboard());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        DataInteraction constraintLayout = onData(anything())
+                .inAdapterView(allOf(withId(R.id.search_results_list),
+                        childAtPosition(
+                                withId(R.id.search_results_container),
+                                1)))
+                .atPosition(0);
+        constraintLayout.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction observableWebView = onView(
+                allOf(withId(R.id.page_web_view),
+                        childAtPosition(
+                                allOf(withId(R.id.page_contents_container),
+                                        childAtPosition(
+                                                withId(R.id.page_fragment),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        observableWebView.perform(longClick());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction imageButton = onView(
+                allOf(withClassName(is("android.widget.ImageButton")), withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                2),
+                        isDisplayed())).inRoot(isPlatformPopup());
+        imageButton.perform(click());
+
+        DataInteraction linearLayout2 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.RelativeLayout")),
+                        0))
+                .atPosition(1).inRoot(isPlatformPopup());
+        linearLayout2.perform(click());
+
+        ViewInteraction actionBar$Tab = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.horizontal_scroll_languages),
+                                0),
+                        1),
+                        isDisplayed()));
+        actionBar$Tab.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                withId(R.id.translate_translated_text));
+
+        textView.check(matches(withText("that")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
