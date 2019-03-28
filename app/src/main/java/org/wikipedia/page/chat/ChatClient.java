e5f8ebf1c6d873792a9a8376b1f326b7728f26bb
zacharybys <zachary.bys@gmail.com>
2019-03-27 22:01:02 -0400

    [#80] fixed async issues with updating the idCount

--- app/src/main/java/org/wikipedia/page/chat/ChatClient.java
+++ app/src/main/java/org/wikipedia/page/chat/ChatClient.java
@@ -1,6 +1,7 @@
 package org.wikipedia.page.chat;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -10,8 +11,10 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.wikipedia.util.log.L;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Each article will have its own instance of ChatClient.
@@ -20,24 +23,30 @@ public class ChatClient {
     final private String userPrefix = "anon";
     final private String messagesPath = "messages";
     final private String articlesPath = "articles";
+    final private String idCountPath = "idCount";
 
     private int idCount;
-
+    private boolean lock;
+    private List<Message> messageQueue;
     private DatabaseReference articlesRef;
 
     public ChatClient(int articleId) {
         this.idCount = 0;
+        closeLock();
+        this.messageQueue = new ArrayList<>();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         this.articlesRef = database.getReference(articlesPath + "/" + articleId);
 
         // Read all data from the article node. Set the instance variables here.
-        this.articlesRef.addValueEventListener(new ValueEventListener() {
+        this.articlesRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.hasChild("idCount")) {
-                    idCount = dataSnapshot.child("idCount").getValue(Integer.class) != null
-                            ? dataSnapshot.child("idCount").getValue(Integer.class) : 0;
+                if (dataSnapshot.hasChild(idCountPath)) {
+                    idCount = dataSnapshot.child(idCountPath).getValue(Integer.class) != null
+                            ? dataSnapshot.child(idCountPath).getValue(Integer.class) : 0;
                 }
+                enterChatRoom();
+                openLock();
             }
 
             @Override
@@ -53,8 +62,24 @@ public class ChatClient {
 
         Message newMessage = new Message(user, message, timeStamp);
 
-        DatabaseReference messagesRef = articlesRef.child(messagesPath);
-        messagesRef.push().setValue(newMessage);
+        writeMessage(newMessage);
+    }
+
+    private void writeMessage(Message message) {
+        if (isLocked()) {
+            messageQueue.add(message);
+        } else {
+            DatabaseReference messagesRef = articlesRef.child(messagesPath);
+            messagesRef.push().setValue(message);
+        }
+    }
+
+    private void consumeMessageQueue() {
+        for (Message message : this.messageQueue) {
+            message.setUser(getUser());
+            writeMessage(message);
+        }
+        this.messageQueue.clear();
     }
 
     public int getIdCount() {
@@ -65,9 +90,22 @@ public class ChatClient {
         return userPrefix + String.valueOf(getIdCount());
     }
 
-    public void enterChatRoom() {
+    private void enterChatRoom() {
         // Update idCount
         this.idCount++;
         this.articlesRef.child("idCount").setValue(this.idCount);
+    }
+
+    private void closeLock() {
+        this.lock = true;
+    }
+
+    private void openLock() {
+        this.lock = false;
+        consumeMessageQueue();
+    }
+
+    private boolean isLocked() {
+        return this.lock;
     }
 }
