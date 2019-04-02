657177ba64177413db5521880f2e7c6e9a760359
Chris Woo <chriskfwoo@gmail.com>
2019-04-02 01:41:59 -0400

    [#82] refactor UI test and fix checkstyle

--- app/src/androidTest/java/org/wikipedia/espresso/onboarding/OnboardNewFeaturesTest.java
+++ app/src/androidTest/java/org/wikipedia/espresso/onboarding/OnboardNewFeaturesTest.java
@@ -23,14 +23,13 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 import static org.wikipedia.espresso.util.ViewTools.waitFor;
 
+@SuppressWarnings("checkstyle:magicnumber")
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class OnboardNewFeaturesTest {
@@ -40,14 +39,8 @@ public class OnboardNewFeaturesTest {
 
     @Test
     public void onBoardNewFeaturesTest() {
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        waitFor(2000);
 
         onView(allOf(withId(R.id.drawer_icon_menu))).perform(click());
 
@@ -60,16 +53,6 @@ public class OnboardNewFeaturesTest {
                                 7),
                         isDisplayed()));
         linearLayout.perform(click());
-
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.view_onboarding_page_primary_text), withText("Image search"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0),
-                        isDisplayed()));
-        textView.check(matches(withText("Image search")));
 
         ViewInteraction textView2 = onView(
                 allOf(withId(R.id.fragment_onboarding_skip_button), withText("Skip"),
@@ -91,16 +74,37 @@ public class OnboardNewFeaturesTest {
                         isDisplayed()));
         imageView.check(matches(isDisplayed()));
 
-        ViewInteraction appCompatImageView = onView(
-                allOf(withId(R.id.fragment_onboarding_forward_button), withContentDescription("Continue"),
+        testFeatureTextDisplayed("Image search");
+        testNextFeatureSwipe();
+        testFeatureTextDisplayed("Text-to-speech");
+        testNextFeatureSwipe();
+        testFeatureTextDisplayed("Selective translation");
+        testNextFeatureSwipe();
+        testFeatureTextDisplayed("No history");
+        testNextFeatureSwipe();
+        testFeatureTextDisplayed("Categories");
+        testNextFeatureSwipe();
+        testFeatureTextDisplayed("Discover");
+        testNextFeatureSwipe();
+        testFeatureTextDisplayed("Chat");
+
+        ViewInteraction imageView2 = onView(allOf(withId(R.id.fragment_onboarding_done_button), isDisplayed()));
+        imageView2.check(matches(isDisplayed()));
+    }
+
+    private void testFeatureTextDisplayed(String textToCheck) {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.view_onboarding_page_primary_text), withText(textToCheck),
                         childAtPosition(
                                 childAtPosition(
-                                        withClassName(is("android.widget.FrameLayout")),
-                                        2),
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        1),
                                 0),
                         isDisplayed()));
-        appCompatImageView.perform(click());
+        textView.check(matches(withText(textToCheck)));
+    }
 
+    private void testNextFeatureSwipe() {
         ViewInteraction rtlViewPager = onView(
                 allOf(withId(R.id.fragment_pager),
                         childAtPosition(
@@ -111,120 +115,6 @@ public class OnboardNewFeaturesTest {
                                 0),
                         isDisplayed()));
         rtlViewPager.perform(swipeLeft());
-
-        ViewInteraction textView3 = onView(
-                allOf(withId(R.id.view_onboarding_page_primary_text), withText("Text-to-speech"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0)));
-        textView3.check(matches(withText("Text-to-speech")));
-
-        ViewInteraction rtlViewPager2 = onView(
-                allOf(withId(R.id.fragment_pager),
-                        childAtPosition(
-                                allOf(withId(R.id.fragment_onboarding_pager_container),
-                                        childAtPosition(
-                                                withId(R.id.fragment_container),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        rtlViewPager2.perform(swipeLeft());
-
-        ViewInteraction textView4 = onView(
-                allOf(withId(R.id.view_onboarding_page_primary_text), withText("Selective translation"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0)));
-        textView4.check(matches(withText("Selective translation")));
-
-        ViewInteraction rtlViewPager3 = onView(
-                allOf(withId(R.id.fragment_pager),
-                        childAtPosition(
-                                allOf(withId(R.id.fragment_onboarding_pager_container),
-                                        childAtPosition(
-                                                withId(R.id.fragment_container),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        rtlViewPager3.perform(swipeLeft());
-
-        ViewInteraction textView5 = onView(
-                allOf(withId(R.id.view_onboarding_page_primary_text), withText("No history"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0)));
-        textView5.check(matches(withText("No history")));
-
-        ViewInteraction rtlViewPager4 = onView(
-                allOf(withId(R.id.fragment_pager),
-                        childAtPosition(
-                                allOf(withId(R.id.fragment_onboarding_pager_container),
-                                        childAtPosition(
-                                                withId(R.id.fragment_container),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        rtlViewPager4.perform(swipeLeft());
-
-        ViewInteraction textView6 = onView(
-                allOf(withId(R.id.view_onboarding_page_primary_text), withText("Categories"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0)));
-        textView6.check(matches(withText("Categories")));
-
-        ViewInteraction rtlViewPager5 = onView(
-                allOf(withId(R.id.fragment_pager),
-                        childAtPosition(
-                                allOf(withId(R.id.fragment_onboarding_pager_container),
-                                        childAtPosition(
-                                                withId(R.id.fragment_container),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        rtlViewPager5.perform(swipeLeft());
-
-        ViewInteraction textView7 = onView(
-                allOf(withId(R.id.view_onboarding_page_primary_text), withText("Discover"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0)));
-        textView7.check(matches(withText("Discover")));
-
-        ViewInteraction rtlViewPager6 = onView(
-                allOf(withId(R.id.fragment_pager),
-                        childAtPosition(
-                                allOf(withId(R.id.fragment_onboarding_pager_container),
-                                        childAtPosition(
-                                                withId(R.id.fragment_container),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        rtlViewPager6.perform(swipeLeft());
-
-        ViewInteraction textView8 = onView(
-                allOf(withId(R.id.view_onboarding_page_primary_text), withText("Chat"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        1),
-                                0),
-                        isDisplayed()));
-        textView8.check(matches(withText("Chat")));
-
-        ViewInteraction imageView2 = onView(
-                allOf(withId(R.id.fragment_onboarding_done_button),isDisplayed()));
-        imageView2.check(matches(isDisplayed()));
     }
 
     private static Matcher<View> childAtPosition(
