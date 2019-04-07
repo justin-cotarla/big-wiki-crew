5481d005dfe295b94de01393bf1d480967f61be5
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-04-07 12:25:38 -0400

    [#72][#112] Remove unused import in ReadingListsFragment

--- app/src/main/java/org/wikipedia/readinglist/ReadingListsFragment.java
+++ app/src/main/java/org/wikipedia/readinglist/ReadingListsFragment.java
@@ -20,7 +20,6 @@ import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
