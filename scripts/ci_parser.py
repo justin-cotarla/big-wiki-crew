d5c41e32e846175590555979c49e28079e641ced
Chris Woo <chriskfwoo@gmail.com>
2019-04-12 16:06:03 -0400

    [#104] setup travis token in script

--- scripts/ci_parser.py
+++ scripts/ci_parser.py
@@ -6,11 +6,8 @@ import argparse
 import json
 
 
-SLACK_URL = "https://hooks.slack.com/services/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"
-
-
 def get_args():
-    """ Python argument parser """
+    """ Python argument parser. """
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--id", help="The job id of travis build.")
@@ -24,36 +21,36 @@ def get_args():
 def get_ci_logs(id, token):
     """ Download travis ci logs
 
-    :params job_id: integer travis ci build number
+    :params job_id: travis ci build number
+    :params token: travis access token 
     :return: text body of the response
     """
 
     try:
         url = "https://api.travis-ci.com/v3/job/{}/log.txt".format(id)
         response = requests.get(url, headers={'Authorization': 'token ' + token})
-        print(response.text)
         if response.status_code != 200 or "Sorry, we experienced an error." in response.text:
             raise Exception
         return response.text
     except Exception as error:
-        slack_webhook(build_id=None, stage_name=None, message="Cannot download Travis CI logs")
+        slack_webhook(build_id=None, stage_name=None, url=url, message="Cannot download Travis CI logs")
         sys.exit("Cannot download Travis CI logs")
 
 
 def parse_logs(response):
-    """ Parses the travis log """
+    """ Parses the travis log. """
 
     # TODO parse the text for summaries
     if "BUILD FAILED" in response:
         return "build failed"
 
-    if "exited with 1" in response:
-        return "error"
-
     return "No errors :)"
 
 
 def slack_webhook(build_id, stage_name, url, message):
+    """ Send summary to slack. """
+
+    SLACK_URL = "https://hooks.slack.com/services/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"
     headers = {'content-type': 'application/json'}
     data = {
         "username": "TRAVIS CI PARSER SUMMARY",
@@ -64,10 +61,6 @@ def slack_webhook(build_id, stage_name, url, message):
 
 if __name__ == "__main__":
     args = get_args()
-    print(args.id)
-    print(args.stage)
-    print(args.url)
-    print(args.token)
     response = get_ci_logs(args.id, args.token)
     message = parse_logs(response)
     slack_webhook(args.id, args.stage, args.url, message)
