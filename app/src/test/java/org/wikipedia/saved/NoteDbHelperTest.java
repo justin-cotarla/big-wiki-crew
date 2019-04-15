2e3cf29a40be4d74937b1a78095612055467d097
Derek Yu <derekyu624@hotmail.com>
2019-04-14 20:00:53 -0400

    [#74] Write tests for new methods in NoteDbHelper
    Co-authored-by: justin-cotarla <justin.cotarla@gmail.com>

--- app/src/test/java/org/wikipedia/saved/NoteDbHelperTest.java
+++ app/src/test/java/org/wikipedia/saved/NoteDbHelperTest.java
@@ -18,23 +18,50 @@ import static org.junit.Assert.assertEquals;
 @Config(application = WikipediaApp.class)
 public class NoteDbHelperTest {
     private NoteDbHelper noteDbHelper;
+    private Note note;
 
     @Before
     public void setup() {
         noteDbHelper = NoteDbHelper.getInstance();
+        this.note = new Note("This is a saved note", new WikiSite("test.wikimedia.org"), "Article Title");
+        noteDbHelper.saveNote(this.note);
+    }
+
+    @Test
+    public void testGetSingleNoteById() {
+        Note note = noteDbHelper.getNoteById(1);
+        assertEquals("This is a saved note", note.content());
+        assertEquals("Article Title", note.title());
+        assertEquals("test.wikimedia.org", note.wiki().authority());
     }
 
     @Test
     public void testGetAllNotes() {
-        WikiSite site = new WikiSite("test.wikimedia.org");
-        Note note = new Note("This is a saved note", site, "Article Title");
-
         List<Note> notes = noteDbHelper.getAllNotes();
-        assertEquals(0, notes.size());
-
-        noteDbHelper.createNote(note);
-        notes = noteDbHelper.getAllNotes();
         assertEquals(1, notes.size());
-        assertEquals(notes.get(0).title(), "Article Title");
+        assertEquals("Article Title", notes.get(0).title());
+    }
+
+    @Test
+    public void testCreateNote() {
+        Note note = new Note("Another saved note", new WikiSite("test.wikimedia.org"), "Another Title");
+        noteDbHelper.saveNote(note);
+
+        assertEquals(2, noteDbHelper.getAllNotes().size());
+    }
+
+    @Test
+    public void testUpdateNote() {
+        this.note.description("This is a test description");
+        noteDbHelper.updateNote(this.note);
+
+        Note updatedNote = noteDbHelper.getNoteById(this.note.id());
+        assertEquals("This is a test description", updatedNote.description());
+    }
+
+    @Test
+    public void testDeleteNote() {
+        noteDbHelper.deleteNote(this.note);
+        assertEquals(0, noteDbHelper.getAllNotes().size());
     }
 }
